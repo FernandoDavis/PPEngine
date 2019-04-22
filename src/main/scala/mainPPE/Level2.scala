7499db1091c696f54b40fe51e526f1a65103663f
GZA <zhaotian.wu@upr.edu>
2019-04-22 09:21:50 -0400

    Stop making new REPOs! Put Level2 Back in

--- src/main/scala/mainPPE/Level2.scala
+++ src/main/scala/mainPPE/Level2.scala
@@ -1,25 +1,54 @@
 package mainPPE
+import java.awt.Color
+
 import Entities.Player
 import Objects.Box
 
 class Level2 extends Level{
+
   var box: Box = new Box(200, 100, 50, 50)
-  var floor: Box = new Box(0, 300, 1000, 20)
-  var box3: Box = new Box(300, 50, 50, 50)
-  var box4: Box = new Box(400, 100, 50, 50)
-  var wall1: Box = new Box(0, 300-50, 50, 500)
-  var wall2: Box = new Box(400, 300-50, 50, 500)
-  var wall3: Box = new Box(1000, 300-500, 50, 500)
-  var wall4: Box = new Box(1000-100, 300-500-50, 50, 500)
+  var floor: Box = new Box(340, 300, 400, 20)
+  floor.setMaskColor(Color.black)
+
+  var Airbox: Box = new Box(400, 100, 50, 50)
+  var Airbox2: Box = new Box(680, 100, 50, 50)
+  Airbox2.setMaskColor(Color.ORANGE)
+  this.StartPosition = new Vector2D(300, 10)
+
+  var platform7: Box = new Box(460, -220, 160, 20)
+  platform7.setMaskColor(Color.GREEN)
+
+  var platform6: Box = new Box(220, -115, 160, 20)
+  platform6.setMaskColor(Color.GREEN)
+
+  var platform5: Box = new Box(300, 10, 220, 20)
+  platform5.setMaskColor(Color.GREEN)
+
+  var platform4: Box = new Box(700, -15, 120, 20)
+  platform4.setMaskColor(Color.BLACK)
+
+  var platform: Box = new Box(850, 350, 250, 20)
+  platform.setMaskColor(Color.BLACK)
+
+  var Slidebox: Box = new Box(550, 40, 50, 50)
+  Slidebox.setMaskColor(Color.green)
+
+  var platform3: Box = new Box(900, 75, 200, 20)
+  platform3.setMaskColor(Color.BLACK)
+
+  var platform2: Box = new Box(1260, 160, 300, 20)
+  platform2.setMaskColor(Color.BLACK)
+
+
 
   override def startLevel() {
     super.startLevel()
-    this.addObject(floor,box4,player,wall1,wall2,wall3,wall4)
-    this.addPlayer(this.getStartPosition)
+    Slidebox.setAnchored(false)
+    this.addObject(floor,Slidebox,Airbox,Airbox2,player,platform,platform2, platform3, platform4, platform5, platform6, platform7)
 
   }
   override def tick(){
     super.tick()
   }
-  
+
 }
