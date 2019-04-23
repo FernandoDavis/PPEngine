437dd543871373d793c237b8db4e3dcf73e16951
Gabriel Soto <gabriel.soto6@upr.edu>
2019-04-22 20:48:26 -0400

    The JComponent (Game) and the current level playing can be accessed from ANY object, this also makes it possible to access the current player globally Implemented some behaviours Objects can now know if they're currently colliding horizontally, this makes it possible to jump Levels can now be reloaded <--needs some more debugging and minor fixes

--- src/main/scala/mainPPE/Behaviour.scala
+++ src/main/scala/mainPPE/Behaviour.scala
@@ -6,21 +6,63 @@ object Behaviour {
   def randomFly(owner: Obj): Behaviour = {
     new Behaviour(owner) {
       owner.setGravity(0)
-      delay=1000
+      delay = 1000
       override def run(): Unit = {
         super.run()
-        if(System.currentTimeMillis()-timer>delay){
-          timer=System.currentTimeMillis()
+        if (System.currentTimeMillis() - timer > delay) {
+          timer = System.currentTimeMillis()
           val rand: Random = new Random()
-          rand.setSeed(System.nanoTime()+System.currentTimeMillis())
-          var x= rand.nextInt(500)
-          if(rand.nextBoolean())
-            x*= -1
-          rand.setSeed(System.nanoTime()+System.currentTimeMillis()+2)
-          var y= rand.nextInt(500)
-          if(rand.nextBoolean())
-            y*= -1
-          this.owner.goTo(this.owner.startPosition+(x,y))
+          rand.setSeed(System.nanoTime() + System.currentTimeMillis())
+          var x = rand.nextInt(500)
+          if (rand.nextBoolean())
+            x *= -1
+          rand.setSeed(System.nanoTime() + System.currentTimeMillis() + 2)
+          var y = rand.nextInt(500)
+          if (rand.nextBoolean())
+            y *= -1
+          this.owner.setTargetPosition(this.owner.startPosition + (x, y))
+        }
+      }
+    }
+  }
+
+  def followPlayer(owner: Obj): Behaviour = {
+    new Behaviour(owner) {
+      delay = 300
+
+      override def run(): Unit = {
+        super.run()
+        if (System.currentTimeMillis() - timer > delay) {
+          timer = System.currentTimeMillis()
+          val player: Obj = Main.getGame.getPlayer
+          if (player != null) {
+            this.owner.setTargetObj(player)
+          }
+          if (owner.leftCollision || owner.rightCollision) {
+            owner.jump(owner.getSpeed*1.2)
+          }
+        }
+      }
+    }
+  }
+
+  def moveBackAndForthH(owner: Obj, distance: Double): Behaviour = {
+    new Behaviour(owner) {
+      delay = 300
+      owner.setTargetDistance(5)
+      var next: Vector2D = new Vector2D(owner.startPosition.getX,owner.getCenterY) + (distance / 2, 0)
+      var h: Int = 1
+      override def run(): Unit = {
+        super.run()
+        if (System.currentTimeMillis() - timer > delay) {
+          timer = System.currentTimeMillis()
+          if (owner.getTargetPos == null) {
+            owner.setTargetPosition(next)
+            h*= -1
+            next =  new Vector2D(owner.startPosition.getX,owner.getCenterY) + (h*distance / 2, 0)
+          }
+          else
+            owner.setTargetPosition(new Vector2D(owner.getTargetPos.getX,owner.getCenterY))
         }
       }
     }
@@ -32,9 +74,10 @@ abstract class Behaviour(var owner: Obj) {
   var delay: Double = 1000
   var timer: Long = 0
 
-  def setOwner(owner: Obj): Unit ={
-    this.owner=owner
+  def setOwner(owner: Obj): Unit = {
+    this.owner = owner
   }
+
   def run(): Unit = {
     if (owner == null)
       return
